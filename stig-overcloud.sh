50f7d3c680edbd3c8dd118d8130c406e9c935b47
Donny Davis <donny@garage.fortnebula.com>
2017-11-30 16:56:06 -0500

    Removed non RHEL Content

--- stig-overcloud.sh
+++ stig-overcloud.sh
@@ -6,25 +6,31 @@ echo "##########################################################################
 read -p "RHN Username: " name
 read -s -p "RHN password: (doesn't echo)" password
 read -p "Subscritpion pool id: " pool_id
+rm -f overcloud-full* ironic-python-agent*
+echo "##########################"
+echo "# Getting Factory Images #"
+echo "##########################"
 for i in /usr/share/rhosp-director-images/overcloud-full-latest-11.0.tar /usr/share/rhosp-director-images/ironic-python-agent-latest-11.0.tar; do tar -xvf $i; done
+sync
+echo "##############################"
+echo "# Creating Partitioned Image #"
+echo "##############################"
+./whole-disk-image.py
+echo "####################################"
+echo "# Subscribing and pulling packages #"
+echo "####################################"
 virt-customize -a overcloud-full.qcow2 --run-command "subscription-manager register --username=$name --password=$password"
 virt-customize -a overcloud-full.qcow2 --run-command "subscription-manager attach --pool $pool_id"
 virt-customize -a overcloud-full.qcow2 --run-command 'subscription-manager repos --disable "*"'
 virt-customize -a overcloud-full.qcow2 --run-command 'subscription-manager repos --enable rhel-7-server-rpms'
 virt-customize -a overcloud-full.qcow2 --run-command 'yum -y install openscap-scanner scap-security-guide aide'
-#############################################
-# The Next command will generate a          #
-# bash script to run on our overcloud image #
-#############################################
+echo "#############################"
+echo "# Hardening Overcloud Image #"
+echo "#############################"
 virt-customize -a overcloud-full.qcow2 --run-command 'oscap xccdf generate fix --template urn:xccdf:fix:script:sh --profile xccdf_org.ssgproject.content_profile_stig-rhel7-disa --output /opt/overcloud-remediation.sh /usr/share/xml/scap/ssg/content/ssg-rhel7-ds.xml'
 sudo LIBGUESTFS_BACKEND=direct  guestmount -a overcloud-full.qcow2 -i /mnt/guest
 sudo cp /mnt/guest/opt/overcloud-remediation.sh .
 sudo guestunmount /mnt/guest
-##############################################
-# I would like to find a better way to       #
-# pull the fips stuff from the stig,         #
-# but I'm in a hurry so this will do for now #
-##############################################
 sudo chown stack:stack overcloud-remediation.sh
 sed -i '/yum -y update/d' overcloud-remediation.sh
 sed -i '/package_command install dracut-fips/,+20 d' overcloud-remediation.sh
@@ -34,8 +40,18 @@ virt-customize -a overcloud-full.qcow2 --upload overcloud-remediation.sh:/opt
 virt-customize -a overcloud-full.qcow2 --run-command 'chmod +x /opt/overcloud-remediation.sh'
 virt-customize -v -a overcloud-full.qcow2 --run-command '/opt/overcloud-remediation.sh'
 virt-customize -a overcloud-full.qcow2 --delete '/opt/overcloud-remediation.sh'
+echo "#########################################"
+echo "# Unregistering and Unsubscribing Image #"
+echo "#########################################"
 virt-customize -a overcloud-full.qcow2 --run-command 'subscription-manager remove --all'
 virt-customize --selinux-relabel -a overcloud-full.qcow2 --run-command 'subscription-manager unregister'
+echo "######################################"
+echo "# Uploading Hardened Image to Glance #"
+echo "######################################"
 source ~/stackrc
 openstack overcloud image upload --update-existing --image-path $(pwd)
+echo "#################################"
+echo "# Your Image is ready to deploy #"
+echo "#################################"
+
 
